cb52b7cd7ee7f323a72f286f865a233664756a35
Sandeep <Sandeep>
2019-12-20 14:49:43 +0530

    Adding Project Files

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// this line tells express to serve
+// js or css files from frontend folder
+app.use(express.static('frontend'))
+
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
+app.get('/login', (req, res) => res.send('Login'))
+app.get('/register', (req, res) => res.send('Register'))
+
+var student = {"name" : "Being Zero",
+               "college" : "VNRVJIET",
+               "regno": "112323232"};
+
+app.get('/data', (req, res) => res.json(student))
+
+//res.sendFile
+//res.json
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
