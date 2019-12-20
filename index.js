f6076564ab2c20401ed087f54641243265895517
Sandeep <Sandeep>
2019-12-20 15:08:30 +0530

    Changes for Heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // this line tells express to serve
 // js or css files from frontend folder
